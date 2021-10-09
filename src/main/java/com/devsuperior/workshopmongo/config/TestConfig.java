103264169e85172f5ae9d3cfd2cdd03e34e0d314
Tiago Gomes <tiagogomes187@gmail.com>
2021-10-09 19:24:22 -0300

    Database seeding

--- src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
+++ src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
@@ -1,5 +1,8 @@
 package com.devsuperior.workshopmongo.config;
 
+import com.devsuperior.workshopmongo.models.embedded.Author;
+import com.devsuperior.workshopmongo.models.embedded.Comment;
+import com.devsuperior.workshopmongo.models.entities.Post;
 import com.devsuperior.workshopmongo.models.entities.User;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
@@ -8,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -22,6 +26,7 @@ public class TestConfig {
 
     @PostConstruct
     public void init(){
+
         userRepository.deleteAll();
         postRepository.deleteAll();
 
@@ -29,8 +34,23 @@ public class TestConfig {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        userRepository.saveAll(Arrays.asList(maria, alex,bob));
+        Post post1 = new Post(null, Instant.parse("2021-02-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+        Post post2 = new Post(null, Instant.parse("2021-02-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+        Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2021-02-13T14:30:01Z"), new Author(alex));
+        Comment c2 = new Comment("Aproveite", Instant.parse("2021-02-13T15:38:05Z"), new Author(bob));
+        Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2021-02-14T12:34:26Z"), new Author(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3,c2));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
+
 
     }
 }
