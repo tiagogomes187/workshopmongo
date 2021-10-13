1c9078bc817ca2f80886bbc1a717145ee3658c3c
Tiago Gomes <tiagogomes187@gmail.com>
2021-10-13 19:32:53 -0300

    GET user by id

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,9 +3,11 @@ package com.devsuperior.workshopmongo.services;
 import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.models.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +22,11 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> list = repository.findAll();
         return list.stream().map(UserDTO::new).collect(Collectors.toList());
+    }
+
+    public UserDTO findById(String id){
+        Optional<User> result = repository.findById(id);
+        User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+        return new UserDTO(entity);
     }
 }
