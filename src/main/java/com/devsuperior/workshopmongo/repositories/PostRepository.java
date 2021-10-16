3586b87d9cbce8846f72dc0b9ddc431c4235f80f
Tiago Gomes <tiagogomes187@gmail.com>
2021-10-16 17:57:23 -0300

    GET Post full search

--- src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 
 @Repository
@@ -12,6 +13,9 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
+
+    @Query("{ $and: [ { 'moment': { $gte: ?1} }, { 'moment': { $lte: ?2} }  , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -6,6 +6,8 @@ import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -34,6 +36,22 @@ public class PostService {
     public List<PostDTO> findByTitle(String text){
         List<Post> list = repository.searchTitle(text);
         return list.stream().map(PostDTO::new).collect(Collectors.toList());
+    }
+
+    public List<PostDTO> fullSearch(String text, String start, String end){
+        Instant startMoment = convertMoment(start, Instant.ofEpochMilli(0L));
+        Instant endMoment = convertMoment(end, Instant.now());
+        List<Post> list = repository.fullSearch(text, startMoment, endMoment);
+        return list.stream().map(PostDTO::new).collect(Collectors.toList());
+    }
+
+    private Instant convertMoment(String originalString, Instant alternative) {
+        try{
+            return Instant.parse(originalString);
+        }
+        catch (DateTimeParseException e){
+            return alternative;
+        }
     }
 
 }
